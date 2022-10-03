33e417e18f1b60db3a4136da953eddae4cccab99
Paul Cox <8927278+cpaulcox@users.noreply.github.com>
2022-10-03 16:54:26 +0100

    refactoring

--- sw.js
+++ sw.js
@@ -1,48 +1,13 @@
-basket_id = ""
-self.addEventListener('activate', function(event) {
-    console.log("Ready for the demo");
-    basket_id = create_UUID();
-});
+// Service worker lifecycle event handling
 
-token = "my access token"
+// Fetch event handling
 
+// The fetch event is fired when the fetch() method is called.
+// Use the event name in methods like addEventListener(), or set an event handler property.
+// addEventListener("fetch", (event) => {});  'fetch' is a predfined hook name.
 
-self.addEventListener('message', function(event) {
-    var data = event.data;
-
-    if (data.command == "oneWayCommunication") {
-        console.log("Message from the Page : ", data.message);
-    } else if (data.command == "twoWayCommunication") {
-        console.log("Responding to message from the Page: ", data.message);
-        event.ports[0].postMessage({
-            "message": "Hi, Page with..." + token
-        });
-        event.ports[0].postMessage({
-            "message": "basket id = " + basket_id
-        });
-    } else if (data.command == "broadcast") {
-        console.log("Broadcasting to the clients");
-
-        self.clients.matchAll().then(function(clients) {
-            clients.forEach(function(client) {
-                client.postMessage({
-                    "command": "broadcastOnRequest",
-                    "message": "This is a broadcast on request from the SW"
-                });
-                client.postMessage({
-                    "command": "broadcastOnRequest",
-                    "message": "Broadcasting new basket with id = "+ basket_id
-                });
-                
-            })
-        })
-    }
-});
-
-
-// Intercepts all fetch traffic. 'fetch' is a predfined hook name.
-self.addEventListener('fetch', event => {
-    
+// Intercepts all fetch traffic.
+onfetch = (event) => { 
     
   console.log('Handling fetch event for', event.request.url);
   console.log('Request details', event.request);
@@ -91,8 +56,46 @@ self.addEventListener('fetch', event => {
 });
 
 
+basket_id = ""
+self.addEventListener('activate', function(event) {
+    console.log("Ready for the demo");
+    basket_id = create_UUID();
+});
+
+token = "my access token"
 
 
+self.addEventListener('message', function(event) {
+    var data = event.data;
+
+    if (data.command == "oneWayCommunication") {
+        console.log("Message from the Page : ", data.message);
+    } else if (data.command == "twoWayCommunication") {
+        console.log("Responding to message from the Page: ", data.message);
+        event.ports[0].postMessage({
+            "message": "Hi, Page with..." + token
+        });
+        event.ports[0].postMessage({
+            "message": "basket id = " + basket_id
+        });
+    } else if (data.command == "broadcast") {
+        console.log("Broadcasting to the clients");
+
+        self.clients.matchAll().then(function(clients) {
+            clients.forEach(function(client) {
+                client.postMessage({
+                    "command": "broadcastOnRequest",
+                    "message": "This is a broadcast on request from the SW"
+                });
+                client.postMessage({
+                    "command": "broadcastOnRequest",
+                    "message": "Broadcasting new basket with id = "+ basket_id
+                });
+                
+            })
+        })
+    }
+});
 
 
 function create_UUID(){
